368e06f242f70b37fa6a555ec302ffce7cbb4f67
이재승/금오공과대학교 <grayhacker91@gmail.com>
2015-11-14 17:28:07 +0900

    rmi example

---
+++ java/src/RMI/RMIClient.java
@@ -0,0 +1,18 @@
+package RMI;
+
+import java.rmi.*;
+
+public class RMIClient {
+	static final String server = "127.0.0.1";
+
+	public static void main(String[] args) {
+		try {
+			String host = 
+					"rmi://" + server + "/DateServer";
+			//get a proxy for the remote object
+			RemoteDate ds = (RemoteDate)Naming.lookup(host);
+			System.out.println("되라");
+			System.out.println(ds.getDate());
+		} catch (Exception e) { }
+	}  
+}
